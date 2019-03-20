6e3c7692df2e1d05d7cf38af85670b33a42d36d1
AndrewWalker <awalker88@me.com>
2019-03-20 11:01:36 -0500

    made sheet names consistent

--- refresh.py
+++ refresh.py
@@ -15,36 +15,36 @@ update_time_cell = 'H15'  # cell that contains time of the last refresh
 gc = pyg.authorize()
 
 workbook: pyg.Spreadsheet = gc.open(spreadsheet_name)
-rankings_sheet: pyg.Worksheet = workbook.worksheet_by_title('Rankings')
-champions_sheet: pyg.Worksheet = workbook.worksheet_by_title('Weekly Champions')
-player_list_sheet: pyg.Worksheet = workbook.worksheet_by_title('Player List')
-game_responses_sheet: pyg.Worksheet = workbook.worksheet_by_title('Game Responses')
-playerID_responses_sheet: pyg.Worksheet = workbook.worksheet_by_title('Player ID Responses')
-skill_history_sheet: pyg.Worksheet = workbook.worksheet_by_title('Skill History')
-game_list_sheet: pyg.Worksheet = workbook.worksheet_by_title('Game List')
+rankings_ss: pyg.Worksheet = workbook.worksheet_by_title('Rankings')
+champion_ss: pyg.Worksheet = workbook.worksheet_by_title('Weekly Champions')
+player_list_ss: pyg.Worksheet = workbook.worksheet_by_title('Player List')
+game_responses_ss: pyg.Worksheet = workbook.worksheet_by_title('Game Responses')
+playerID_responses_ss: pyg.Worksheet = workbook.worksheet_by_title('Player ID Responses')
+skill_history_ss: pyg.Worksheet = workbook.worksheet_by_title('Skill History')
+game_list_ss: pyg.Worksheet = workbook.worksheet_by_title('Game List')
 
 # add new players
-sheets_interface.add_new_players(playerID_responses_sheet, h)
+sheets_interface.add_new_players(playerID_responses_ss, h)
 
 # add new games
-new_games = sheets_interface.get_new_game_responses(game_responses_sheet, h.roster)
+new_games = sheets_interface.get_new_game_responses(game_responses_ss, h.roster)
 sheets_interface.add_new_game_responses(new_games, h)
 
 # update Rankings page
-sheets_interface.update_rankings(rankings_sheet, h, frc, lrc)
+sheets_interface.update_rankings(rankings_ss, h, frc, lrc)
 
 # update Skill History page
-sheets_interface.update_skill_history(skill_history_sheet, date(2019, 3, 11), h)
+sheets_interface.update_skill_history(skill_history_ss, date(2019, 3, 11), h)
 
 # update Champions page
-sheets_interface.update_champions_list(champions_sheet, rankings_sheet, frc, lrc)
+sheets_interface.update_champions_list(champion_ss, rankings_ss, frc, lrc)
 
 # update Player List page
-sheets_interface.update_player_list(player_list_sheet, h)
+sheets_interface.update_player_list(player_list_ss, h)
 
 # update Game List page
-sheets_interface.update_game_list(game_list_sheet, h)
+sheets_interface.update_game_list(game_list_ss, h)
 
 # update 'Last Updated' cell
 now = datetime.now()
-rankings_sheet.update_value(update_time_cell, f'{now.strftime("%m-%d-%Y %H:%M:%S")}')
+rankings_ss.update_value(update_time_cell, f'{now.strftime("%m-%d-%Y %H:%M:%S")}')
--- sheets_interface.py
+++ sheets_interface.py
@@ -6,9 +6,9 @@ from time import sleep
 from datetime import date, timedelta
 
 
-def get_new_game_responses(game_responses_sht, roster):
+def get_new_game_responses(game_responses_ss, roster):
     """ Pulls all responses from sheet, figures out which ones are new, and returns thm formatted for adding to History
-    :param game_responses_sht: worksheet to pull responses from
+    :param game_responses_ss: worksheet to pull responses from
     :param roster: roster of players from History class
     :returns: list of new responses that are easily read by add_game(): [[team_one], [team_two], team_one_score,
                                                                           team_two_score, timestamp, notes]
@@ -21,7 +21,7 @@ def get_new_game_responses(game_responses_sht, roster):
     previous = pkl.load(open('previous_game_responses.pkl', 'rb'))
 
     # load in all submissions in google sheet
-    current = pd.DataFrame(game_responses_sht.get_all_values())
+    current = pd.DataFrame(game_responses_ss.get_all_values())
     current.columns = ['timestamp', 'team_one', 'team_two', 'team_one_score', 'team_two_score', 'notes']
     current = current[current.timestamp != '']  # remove blank rows
     current = current[current.timestamp != 'Timestamp']  # gets rid of sheet header row
@@ -75,10 +75,10 @@ def add_new_game_responses(new_game_responses, history):
             history.add_game(response[0], response[1], response[2], response[3], response[4])
 
 
-def add_new_players(player_responses_sht, history):
+def add_new_players(player_responses_ss, history):
     """
     Retrieves and checks new playerID requests
-    :param player_responses_sht:
+    :param player_responses_ss:
     :param history: History class that we pull the roster from
     :return: list of new [timestamp, name]s
     """
@@ -89,7 +89,7 @@ def add_new_players(player_responses_sht, history):
     previous = pkl.load(open('previous_playerID_responses.pkl', 'rb'))
 
     # load in new playerID submissions
-    current = pd.DataFrame(player_responses_sht.get_all_values())
+    current = pd.DataFrame(player_responses_ss.get_all_values())
     current.columns = ['timestamp', 'name']
     current = current[current.timestamp != '']  # remove blank rows
     current = current[current.timestamp != 'Timestamp']  # gets rid of sheet header row
@@ -114,10 +114,10 @@ def add_new_players(player_responses_sht, history):
     pkl.dump(combined, open('previous_playerID_responses.pkl', 'wb'))
 
 
-def update_rankings(rankings_sht, h, first_rankings_cell, last_rankings_cell):
+def update_rankings(rankings_ss, h, first_rankings_cell, last_rankings_cell):
     """
-    Gets top players in a history's roster and updates the ranking table in rankings_sht
-    :param rankings_sht: google sheet with rankings table
+    Gets top players in a history's roster and updates the ranking table in rankings_ss
+    :param rankings_ss: google sheet with rankings table
     :param h: History class with roster you want to pull from
     :param first_rankings_cell: top left cell of your rankings table
     :param last_rankings_cell: top right cell of your rankings table
@@ -134,17 +134,17 @@ def update_rankings(rankings_sht, h, first_rankings_cell, last_rankings_cell):
         lst.insert(0, i + 1)
         lst[1], lst[2], lst[3] = lst[2], lst[3], lst[1]  # format for output to chart
 
-    rankings_sht.clear(first_rankings_cell, last_rankings_cell[0] + '1000')  # clears range to paste player rankings
+    rankings_ss.clear(first_rankings_cell, last_rankings_cell[0] + '1000')  # clears range to paste player rankings
     model_cell = pyg.Cell('A1')
-    rankings_sht.get_values(first_rankings_cell, last_rankings_cell[0] + '1000', returnas='range') \
+    rankings_ss.get_values(first_rankings_cell, last_rankings_cell[0] + '1000', returnas='range') \
         .apply_format(model_cell)  # format range to be blank
-    rankings_sht.update_values(first_rankings_cell, ranking_list)  # TODO: Fix when there are no players and rl is empty
+    rankings_ss.update_values(first_rankings_cell, ranking_list)  # TODO: Fix when there are no players and rl is empty
 
 
-def update_skill_history(skill_history_sht, start_date, h):
+def update_skill_history(skill_history_ss, start_date, h):
     """
     updates skill history sheet with new dates and players
-    :param skill_history_sht: google sheet to update
+    :param skill_history_ss: google sheet to update
     :param start_date: date you want to first track skill history from
     :param h: History class containing players and games
     :return: None
@@ -174,23 +174,23 @@ def update_skill_history(skill_history_sht, start_date, h):
             else:
                 date_df[col][index] = latest_ranking
 
-    skill_history_sht.clear()
-    skill_history_sht.update_values('A1', [date_df.columns.values.tolist()])
-    skill_history_sht.update_values('A2', date_df.values.tolist())
+    skill_history_ss.clear()
+    skill_history_ss.update_values('A1', [date_df.columns.values.tolist()])
+    skill_history_ss.update_values('A2', date_df.values.tolist())
 
 
-def update_champions_list(champions_sheet, rankings_sheet, first_rankings_cell, last_rankings_cell):
+def update_champions_list(champions_ss, rankings_ss, first_rankings_cell, last_rankings_cell):
     """
     if date when run is 7 or more days since latest entry in Previous Champions sheet, adds champion based on current
     champion
-    :param champions_sheet: sheet with previous champions
-    :param rankings_sheet: sheet with current rankings
+    :param champions_ss: sheet with previous champions
+    :param rankings_ss: sheet with current rankings
     :param first_rankings_cell: cells to pull champion info from
     :param last_rankings_cell: cells to pull champion info from
     :return: None
     """
     # update champion list
-    current = pd.DataFrame(champions_sheet.get_all_values())
+    current = pd.DataFrame(champions_ss.get_all_values())
     current = current[1:]  # gets rid of header row from sheet
     current.columns = ['week', 'champion', 'playerID', 'rating']
     current = current[current.week != '']  # remove blank rows
@@ -202,11 +202,11 @@ def update_champions_list(champions_sheet, rankings_sheet, first_rankings_cell, 
         no_players = False
         top_player_is_zero = False
         # checks if a champion even can be crowned
-        if rankings_sheet.get_value(first_rankings_cell) == '':
+        if rankings_ss.get_value(first_rankings_cell) == '':
             no_players = True
             print('No players in Rankings sheet. No champion.')
 
-        if int(rankings_sheet.get_value(last_rankings_cell)) == 0:
+        if int(rankings_ss.get_value(last_rankings_cell)) == 0:
             print('Top player has not played any games. No champion.')
             top_player_is_zero = True
 
@@ -215,19 +215,19 @@ def update_champions_list(champions_sheet, rankings_sheet, first_rankings_cell, 
         if no_players or top_player_is_zero:
             new_champion = [[last_monday, 'No champion', 'No champion', 'NA']]
         else:
-            new_champion = rankings_sheet.get_values(first_rankings_cell, last_rankings_cell[0] + first_rankings_cell[1:])
+            new_champion = rankings_ss.get_values(first_rankings_cell, last_rankings_cell[0] + first_rankings_cell[1:])
         new_champion[0][0] = f"{last_monday[5:7]}/{last_monday[8:10]}/{last_monday[2:4]}"
         combined = pd.concat([current, pd.DataFrame(new_champion, columns=['week', 'champion', 'playerID', 'rating'])])
-        champions_sheet.update_values('A2', combined.values.tolist())
+        champions_ss.update_values('A2', combined.values.tolist())
         print(f"New Champion added: {new_champion}")
     else:
         print("Not time for a new champion yet.")
 
 
-def update_player_list(player_list_sheet: pyg.Worksheet, h):
+def update_player_list(player_list_ss: pyg.Worksheet, h):
     """
 
-    :param player_list_sheet: google sheet you want to update
+    :param player_list_ss: google sheet you want to update
     :param h: History class with roster of players
     :return: None
     """
@@ -253,8 +253,8 @@ def update_player_list(player_list_sheet: pyg.Worksheet, h):
                             h[playerID].current_losing_streak,
                             h[playerID].longest_losing_streak
                             ])
-    player_list_sheet.clear('A1', 'P101')
-    player_list_sheet.update_values('A1', roster_list)
+    player_list_ss.clear('A1', 'P101')
+    player_list_ss.update_values('A1', roster_list)
 
 
 def update_game_list(game_list_sheet, history):
